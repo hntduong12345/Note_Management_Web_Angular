656a25f5b97098faa05b510bfa2b933f2888628b
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-26 20:47:15 +0700

    feat: add note editor feature

---
+++ src/app/pages/note-editor/note-editor.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-editor',
+  imports: [],
+  template: ` <p>note-editor works!</p> `,
+  styles: ``,
+})
+export class NoteEditor {}
