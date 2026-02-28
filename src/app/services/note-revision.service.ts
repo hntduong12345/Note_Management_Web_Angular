797a30aeb3a72abe8a869d43189f139970f4d141
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-28 19:28:09 +0700

    feat: add note revision services api call

--- src/app/services/note-revision.service.ts
+++ src/app/services/note-revision.service.ts
@@ -12,4 +12,41 @@ import {
 @Injectable({
   providedIn: 'root',
 })
-export class NoteRevisionService {}
+export class NoteRevisionService {
+  private http = inject(HttpClient);
+  private authService = inject(AuthService);
+  private readonly API_URL = 'http://localhost:8080/v1/api/note-revisions';
+
+  private get userId(): string {
+    const id = this.authService.currentUser()?.id;
+    if (!id) {
+      console.warn('The user id is not available');
+    }
+    return id ?? '';
+  }
+
+  // Note Revision Functions
+  getRevisionHistory(noteId: string): Observable<NoteRevisionResponse[]> {
+    const params = new HttpParams().set('noteId', noteId).set('userId', this.userId);
+
+    return this.http.get<NoteRevisionResponse[]>(this.API_URL, { params });
+  }
+
+  getRevisionDetail(revisionId: string): Observable<NoteRevisionDetailResponse> {
+    const params = new HttpParams().set('userId', this.userId);
+
+    return this.http.get<NoteRevisionDetailResponse>(`${this.API_URL}/${revisionId}`, { params });
+  }
+
+  createRevision(noteId: string, request: NoteRevisionRequest): Observable<void> {
+    const params = new HttpParams().set('noteId', noteId).set('userId', this.userId);
+
+    return this.http.post<void>(this.API_URL, request, { params });
+  }
+
+  restoreRevision(revisionId: string): Observable<NoteResponse> {
+    const params = new HttpParams().set('userId', this.userId);
+
+    return this.http.put<NoteResponse>(`${this.API_URL}/${revisionId}`, {}, { params });
+  }
+}
