5c9a507fd3c4a37bc8c532f466881d8698b37515
hntduong12345 <115139619+hntduong12345@users.noreply.github.com>
2026-02-28 19:39:12 +0700

    feat: add user service api calls

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { UserResponse } from '../dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {}
+export class UserService {
+  private http = inject(HttpClient);
+  private authService = inject(AuthService);
+  private readonly API_URL = 'http://localhost:8080/v1/api/users';
+
+  // Get userId function
+  private get userId(): string {
+    const id = this.authService.currentUser()?.id;
+    if (!id) {
+      console.warn('The user id is not available');
+    }
+    return id ?? '';
+  }
+
+  // User Functions
+  getUserById(id: string): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`${this.API_URL}/${id}`);
+  }
+
+  getUserByEmail(email: string): Observable<UserResponse> {
+    const params = new HttpParams().set('email', email);
+
+    return this.http.get<UserResponse>(`${this.API_URL}/email`, { params });
+  }
+}
